5a14ba3fc5cb72084bf33db35f354055db63c450
dima <dimitrybogov@gmail.com>
2026-03-25 10:36:40 +0100

    fini exo3a l'agent 1 fait un demi-tour et ensuite fini la course en marhce arrière

--- src/agents/team1/agent1.py
+++ src/agents/team1/agent1.py
@@ -1,16 +1,10 @@
-from pathlib import Path
 import numpy as np
 import random
-from utils.track_utils import compute_curvature, compute_slope
-from omegaconf import OmegaConf
+import math
 
+from utils.track_utils import compute_curvature, compute_slope
 from agents.kart_agent import KartAgent
-from agents.team1.agent_center import AgentCenter
-from agents.team1.agent_speed import AgentSpeed
-from agents.team1.agent_obstacles import AgentObstacles
-from agents.team1.agent_rescue import AgentRescue
-from agents.team1.agent_items import AgentItems
-from agents.team1.agent_drift import AgentDrift
+
 
 class Agent1(KartAgent):
     def __init__(self, env, path_lookahead=3):
@@ -19,25 +13,66 @@ class Agent1(KartAgent):
         self.agent_positions = []
         self.obs = None
         self.isEnd = False
-        self.name = "Tasty Crousteam"
-
-        path_conf = Path(__file__).resolve().parent
-        path_conf = str(path_conf) + '/ConfigFileTeam1.yaml'   #Chemin du fichier de configuration
-        self.conf = OmegaConf.load(path_conf)                           #Importation du fichier de configuration
-
-        self.agentCenter = AgentCenter(env, self.conf, self.path_lookahead)
-        self.agentSpeed = AgentSpeed(env, self.conf, self.agentCenter, self.path_lookahead)
-        self.agentObstacles = AgentObstacles(env, self.conf, self.agentSpeed, self.path_lookahead)
-        self.agentRescue = AgentRescue(env, self.conf, self.agentObstacles)
-        self.agentItems = AgentItems(env, self.conf, self.agentRescue)
-        self.AgentDrift = AgentDrift(env, self.conf, self.agentItems)
+        self.name = "Bohov Dmytro" # replace with your chosen name
+        self.prev_err = 0.0
+        self.step_counter = 0 
 
     def reset(self):
         self.obs, _ = self.env.reset()
         self.agent_positions = []
+        self.prev_err = 0.0
 
     def endOfTrack(self):
         return self.isEnd
 
     def choose_action(self, obs):
-        return self.agentItems.choose_action(obs)
+        if self.isEnd is False: # si le kart n'a pas fini la course
+            print(f"step number : {self.step_counter}") # affiche le nombre de pas sur le terminal
+            self.step_counter += 1 # incremente de 1 à chaque appel de la fonction
+        else:
+            print(f"Fini en {self.step_counter}") # affiche le nombre de pas pour finir la course
+
+        if self.step_counter < 100: # on tourne tant qu'on a pas fait le demi tour
+            steer = -1 # on tourne complétement à gauche
+            acceleration = 0.11 # on laisse l'accelaration à 11%
+            
+            action = {
+                "acceleration": acceleration,
+                "steer": steer,
+                "brake": False, # on fait pas de brake
+                "drift": False, # on fait pas de drift
+                "nitro": False, # on fait pas de nitro
+                "rescue":False, # on fait pas de rescue
+                "fire": False, # on fait pas de fire
+            }
+            return action
+        else: # on fait marche arrière jusqu'à la fin
+            speed = math.sqrt(obs["velocity"][0]**2 + obs["velocity"][2]**2) # on calcule la vitesse avec la formule sqrt(x^2 + z^2)
+            idx = int(speed / 12.0) + 1 # on prend le nœuds par rapport à la vitesse
+
+            target_ctr_x = (obs["paths_start"][idx][0] + obs["paths_end"][idx][0]) / 2.0 # on prend l'x entre le début et fin d'un nœud
+            target_ctr_z = (obs["paths_start"][idx][2] + obs["paths_end"][idx][2]) / 2.0 # on fait pareil pour le z
+
+
+            err = math.atan2(target_ctr_x, target_ctr_z) # angle entre x cible et z cible , 
+            p_k = 0.8 # le "poids" ou l'importance qu'on donne à l'erreur
+            d_k = 1.5 # pareil ici
+            drv = err - self.prev_err # on calcule la difference entre l'angle précedante et actuelle
+            self.prev_err = err # l'angle actuelle devient l'angle précedent pour le prochain pas de temps
+            steer = max(-1.0, min(p_k * err + d_k * drv, 1.0)) # on fait un "clamp" entre les valeurs -1 et 1
+
+            acceleration = 0.0 # on laisse l'accelaration à 0%
+            brake = True # et on fait le marche arrière
+
+            action = {
+                "acceleration": acceleration,
+                "steer": steer,
+                "brake": brake, 
+                "drift": False, # on fait pas de drift
+                "nitro": False, # on fait pas de nitro
+                "rescue":False, # on fait pas de rescue
+                "fire": False, # on fait pas de fire
+            }
+
+            return action
+
