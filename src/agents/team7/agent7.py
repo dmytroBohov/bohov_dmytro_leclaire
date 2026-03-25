280749168a5164b9f1943d316c7c774d9a02352e
dima <dimitrybogov@gmail.com>
2026-03-25 10:04:46 +0100

    fini exo2b après 200 pas l'agent fait marche arrière jusqu'au début

--- src/agents/team7/agent7.py
+++ src/agents/team7/agent7.py
@@ -57,10 +57,27 @@ class Agent7(KartAgent):
             }
             return action
         else: # après 200 pas on fait marche arrière
+            speed = math.sqrt(obs["velocity"][0]**2 + obs["velocity"][2]**2) # on calcule la vitesse avec la formule sqrt(x^2 + z^2)
+            idx = int(speed / 12.0) - 1 # on prend le nœuds par rapport à la vitesse 
+
+            target_ctr_x = (obs["paths_start"][idx][0] + obs["paths_end"][idx][0]) / 2.0 # on prend l'x entre le début et fin d'un nœud
+            target_ctr_z = (obs["paths_start"][idx][2] + obs["paths_end"][idx][2]) / 2.0 # on fait pareil pour le z
+
+
+            err = math.atan2(target_ctr_x, target_ctr_z) # angle entre x cible et z cible , 
+            p_k = 0.8 # le "poids" ou l'importance qu'on donne à l'erreur
+            d_k = 1.5 # pareil ici
+            drv = err - self.prev_err # on calcule la difference entre l'angle précedante et actuelle
+            self.prev_err = err # l'angle actuelle devient l'angle précedent pour le prochain pas de temps
+            steer = max(-1.0, min(p_k * err + d_k * drv, 1.0)) # on fait un "clamp" entre les valeurs -1 et 1
+
+            acceleration = 0.0 # on laisse l'accelaration à 0%
+            brake = True # et on fait le marche arrière
+
             action = {
-                "acceleration": False,
-                "steer": False,
-                "brake": False, # on fait pas de brake
+                "acceleration": acceleration,
+                "steer": steer,
+                "brake": brake, 
                 "drift": False, # on fait pas de drift
                 "nitro": False, # on fait pas de nitro
                 "rescue":False, # on fait pas de rescue
